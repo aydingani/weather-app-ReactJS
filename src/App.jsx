a278551e8544d2454e758c9aa831c602c07e7f84
Aydin Gani <73241817+aydingani@users.noreply.github.com>
2023-05-10 21:43:08 +0400

    added the error message

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   const API_KEY = "af907020cc1bc617fb6b8e500b74fef9";
   const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const options = { day: "numeric", month: "long", year: "numeric" };
+  const today = new Date().toLocaleDateString("en-US", options);
 
   const handleCityChange = (e) => {
     setCity(e.target.value);
@@ -15,21 +19,35 @@ function App() {
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
     );
     const data = await res.json();
-    setWeatherData(data);
+    console.log(data);
+    if (data.cod == "404") {
+      setWeatherData(null);
+      setErrorMessage(
+        "City not found. Please check your spelling and try again."
+      );
+    } else {
+      setWeatherData(data);
+      setErrorMessage("");
+    }
   };
 
   return (
     <div>
-      <p>Weather App is loading...</p>
       <input type="text" onChange={handleCityChange} />
       <button onClick={handleGetWeather}>Get Weather</button>
+      {errorMessage && <p>{errorMessage}</p>}
       {weatherData && (
         <div>
-          <h1>{weatherData.name}</h1>
+          <h1>
+            {weatherData.name}, {weatherData.sys.country}
+          </h1>
+          <h2>{today}</h2>
           <p>
             Temperature: {Math.round(weatherData.main.temp - 273.15)}°C /{" "}
             {Math.round(((weatherData.main.temp - 273.15) * 9) / 5 + 32)}°F
           </p>
+          <p>Humidity: {weatherData.main.humidity}%</p>
+          <p>Wind: {weatherData.wind.speed}km/h</p>
         </div>
       )}
     </div>
